e85c385b9683d8dd5458a903fc50e67f5755c748
KiiT24 <112383715+KiiT24@users.noreply.github.com>
2023-02-20 08:28:31 +0100

    cat

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print 0, 1, - 9
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
 {
-int num = 0;
+	int i;
 
-while (num <= 9)
-{
-putchar(num + 48);
-
-if (num != 9)
-{
-putchar(,);
-putchar( );
-}
-
-++num;
-}
-putchar(n);
-
-return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
