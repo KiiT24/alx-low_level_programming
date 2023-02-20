5577bff245e017a8d48ea642cb35dfd3b4986e0a
KiiT24 <112383715+KiiT24@users.noreply.github.com>
2023-02-20 08:25:55 +0100

    cat

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,25 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print 0-9 using putchar
- *              while using int variable
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
 {
-int num = 0;
+	int i;
 
-while (num <= 9)
-{
-/*convert digit to ASCII representation*/
-putchar(num + 0);
-++num;
-}
-putchar(n);
-
-return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
