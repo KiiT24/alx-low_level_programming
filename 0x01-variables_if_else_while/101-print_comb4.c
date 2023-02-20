b9d4528fce8be1fb3119ccbcb8fde482233087ae
KiiT24 <112383715+KiiT24@users.noreply.github.com>
2023-02-20 08:32:27 +0100

    cat

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
-
 /**
- * main - program that prints all possible different combinations of
- * three digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
  */
-
 int main(void)
 {
-int i = 48;
-int j;
-int k;
-int cal = 44;
+	int i, j, k;
 
-while (i <= 57)
-{
-j = i + 1;
-while (j <= 57)
-{
-k = j + 1;
-while (k <= 57)
-{
-putchar (i);
-putchar (j);
-putchar (k);
-if (i != 55 || j != 56 || k != 57)
-{
-putchar (cal);
-putchar (32);
-}
-k += 1;
-}
-j += 1;
-}
-i += 1;
-}
-putchar (n);
-return (0);
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{
+			for (k = 50; k < 58; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
